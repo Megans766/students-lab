bcf151bb1ca2c6fd18a7584ee4af2a6336214ba4
Megan Smith <megans766@gmail.com>
2023-01-24 16:50:48 -0500

    update files

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,10 +1,16 @@
-
+import Score from "./Score"
 
 const Student = (props) => {
   return (
     <>
-      <h2>Student name: {props.student.name}</h2>
+      <h2>i am a student</h2>
+      {/* <h2>Student name: {props.student.name}</h2>
       <p>Student Bio: {props.student.bio}</p>
+      <Score /> */}
+
+      {/* <h2>
+        {props.studentData.map(student => <studentData student={student} />)}
+      </h2> */}
     </>
   )
 }
